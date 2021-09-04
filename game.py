d95b0a14fc7275d3c358e5713094ec4a6ae4e474
tuuunnn <sstickman0908@gmail.com>
2021-09-04 15:41:14 +0900

    first commit

---
+++ game.py
@@ -0,0 +1,15 @@
+
+def nem(num):
+    prime=[]
+    for i in range(2,num+1):
+        cut = 0
+        for j in range(2, i+1):
+            if i%j==0:
+                cut+=1
+            if cut>1:
+                prime.append(i)
+  
+         
+                return prime
+print(prime[1000])
+
